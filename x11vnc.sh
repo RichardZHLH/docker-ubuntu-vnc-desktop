66f8d4e98b68199be00d9684eba7a3fdb6546a67
Zhang Lihua <zhang.lihua@wanglutech.com>
2019-12-11 06:54:22 +0000

    update x11vnc

--- x11vnc.sh
+++ x11vnc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -f $HOME/.vnc/passwd ]; then
-	x11vnc -display :1 -xkb -forever -shared -repeat  -rfbauth %HOME%/.vnc/passwd
+	x11vnc -display :1 -xkb -forever -shared -repeat  -rfbauth $HOME/.vnc/passwd
 else
 	x11vnc -display :1 -xkb -forever -shared -repeat
 fi
